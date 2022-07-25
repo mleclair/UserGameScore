adc8ef79f9e9a3fb1923cfc7b357567acaa707bb
Michael LeClair <mleclair797@gmail.com>
2022-07-25 18:07:55 -0400

    docs(api): Add summary and description to swagger documentation.

--- user-game-score/Program.cs
+++ user-game-score/Program.cs
@@ -1,4 +1,5 @@
 using GameScore.Repositories;
+using Swashbuckle.AspNetCore.Annotations;
 using UserGameScore.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -9,6 +10,10 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddMemoryCache();
 builder.Services.AddScoped<IGameScoreRepository, GameScoreRepository>();
+builder.Services.AddSwaggerGen(swagger =>
+{
+	swagger.EnableAnnotations();
+});
 
 var app = builder.Build();
 
@@ -31,26 +36,30 @@ var games = new[]
 using var scope = app.Services.CreateScope();
 _gameScoreRepository = scope.ServiceProvider.GetRequiredService<IGameScoreRepository>();
 
-app.MapGet("/usergamescores/{userId}", (int userId) =>
-{
+app.MapGet("/usergamescores/{userId}",
+	[SwaggerOperation(Summary = "Gets user game scores", Description = "Gets the latest score all games of the provided user.")]
+	(int userId) => {
 	return _gameScoreRepository.GetUserGameScores(userId);
 })
 .WithName("GetUserGameScores");
 
-app.MapPost("/usergamescore/{userId}", (int userId) =>
-{
+app.MapPost("/usergamescore/{userId}",
+	[SwaggerOperation(Summary = "Creates new user game", Description = "Creates the initial score for the provided user and game.")]
+	(int userId) => {
 	return _gameScoreRepository.CreateUserGameScore(new UserGameScoreRecord(userId, games[1], 1, 0));
 })
 .WithName("PostUserGameScore");
 
-app.MapPut("/usergamescore/{userId}", (int userId) =>
-{
+app.MapPut("/usergamescore/{userId}",
+	[SwaggerOperation(Summary = "Updates user game score", Description = "Updates the current game score of the provided user and game.")]
+	(int userId) => {
 	return _gameScoreRepository.UpdateUserGameScore(new UserGameScoreRecord(userId, games[1], 1, 100));
 })
 .WithName("UpdateGameScore");
 
-app.MapDelete("/usergamescore/{userId}", (int userId) =>
-{
+app.MapDelete("/usergamescore/{userId}",
+	[SwaggerOperation(Summary = "Delete user game score", Description = "Deletes the game score of the provided user and game.")]
+	(int userId) => {
 	return _gameScoreRepository.DeleteUserGameScore(userId, games[1], 1);
 })
 .WithName("DeleteUserGameScore");
